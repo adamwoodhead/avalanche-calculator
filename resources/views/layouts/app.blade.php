7b9086e23cd2336799158c21cda3b84f8d120360
adamwoodhead <github@adamwoodhead.co.uk>
2021-02-15 15:48:46 +0000

    page titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }} | {{ ucwords(str_replace('.', ' - ', str_replace('-', ' ', str_replace('.show', '', Route::currentRouteName())))) }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -41,8 +41,11 @@
             </main>
         </div>
 
+
+        <!-- Modals -->
         @stack('modals')
 
+        <!-- Final Scripts -->
         @stack('finalscripts')
 
         @livewireScripts
