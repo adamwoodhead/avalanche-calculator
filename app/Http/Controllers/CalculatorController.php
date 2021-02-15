d5294d4d26ff51ab905ace4fcaf182e3cf22cdda
adamwoodhead <githib@adamwoodhead.co.uk>
2021-02-15 03:45:52 +0000

    added missing expected controllers & views

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class CalculatorController extends Controller
 {
-    //
+    public function avalanche(){
+        return View::make('calculators.avalanche');
+    }
+    
+    public function snowball(){
+        return View::make('calculators.snowball');
+    }
 }
