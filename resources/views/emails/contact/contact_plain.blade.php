af8161912b09e4499939c6a6284e9c802eaf0ab2
adamwoodhead <github@adamwoodhead.co.uk>
2021-02-16 19:03:46 +0000

    contact mail plain text

--- resources/views/emails/contact/contact_plain.blade.php
+++ resources/views/emails/contact/contact_plain.blade.php
@@ -3,5 +3,9 @@
         {{ $first_name }}
     </x-slot>
 
-    
+    First Name: {{ $first_name }}
+    Last Name: {{ $last_name }}
+    Email: {{ $email }}
+    Subject: {{ $subject }}
+    Message: {{ $body_message }}
 </x-email-plain-layout>
