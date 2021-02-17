0e6be60712d997f8895167208114be69058624a5
adamwoodhead <github@adamwoodhead.co.uk>
2021-02-17 00:10:52 +0000

    basic debts layout

--- resources/views/livewire/debt-collection-card.blade.php
+++ resources/views/livewire/debt-collection-card.blade.php
@@ -2,6 +2,7 @@
     <div class="flex flex-row justify-between">
         <div class="flex flex-col">
             <h4 class="text-lg">{{ $collection->name }}</h4>
+            <input type="text-lg" wire:model="collection.name">
             <p class="text-gray-700">{{ $collection->description }}</p>
         </div>
         <div class="flex flex-col items-end">
@@ -17,26 +18,34 @@
         </div>
     </div>
 
-    <table class="w-full text-left table-fixed mt-2 border border-solid border-gray-300">
-        <thead class="bg-gray-200">
-            <tr>
-                <th class="w-1/3">Name</th>
-                <th class="w-1/4">Type</th>
-                <th class="w-1/4">Balance</th>
-                <th class="w-1/4">APR</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($collection->debts as $debt)
-            <tr class="hover:bg-gray-100 border border-solid border-gray-200">
-                <td class="truncate">{{ $debt->name }}</td>
-                <td>{{ $debt->debt_type_text }}</td>
-                <td>£{{ number_format($debt->opening_balance, 2) }}</td>
-                <td>{{ str_pad(number_format($debt->interest_rate, 2), 5, '0', STR_PAD_LEFT) }}%</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="relative">
+        <table class="w-full text-left table-fixed mt-2 border border-solid border-gray-300">
+            <thead class="bg-gray-200">
+                <tr>
+                    <th class="w-1/3">Name</th>
+                    <th class="w-1/4">Type</th>
+                    <th class="w-1/4">Balance</th>
+                    <th class="w-1/4">APR</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($collection->debts as $debt)
+                <tr class="hover:bg-gray-100 border border-solid border-gray-200">
+                    <td class="truncate">{{ $debt->name }}</td>
+                    <td>{{ $debt->debt_type_text }}</td>
+                    <td>£{{ number_format($debt->opening_balance, 2) }}</td>
+                    <td>{{ str_pad(number_format($debt->interest_rate, 2), 5, '0', STR_PAD_LEFT) }}%</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <a class="absolute z-10 -bottom-4 left-1 cursor-pointer">
+            <svg class="h-6 w-6 bg-white rounded-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+        </a>
+    </div>
 
     <div class="flex flex-row-reverse mt-4">
         <div class="items-center">
