72f4e86262f7e9e73309385169817607cd4ba468
adamwoodhead <github@adamwoodhead.co.uk>
2021-06-17 16:15:14 +0100

    added saved calculations section to the calculation view of each type

--- resources/views/calculators/avalanche.blade.php
+++ resources/views/calculators/avalanche.blade.php
@@ -1,18 +1,55 @@
 <x-app-layout>
     <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm">
-        <h2 class="text-lg font-semibold mb-4">Avalanche Calculator</h2>
-        <div class="flex flex-col space-y-2">
+        <h2 class="text-lg font-semibold">Avalanche Calculator</h2>
+        <div class="w-full mt-4 flex flex-col space-y-2">
             <p>The Avalanche method of paying off debt is seen as the absolute most efficient method possible, and can be found in many professionally created Debt Management Plans.</p>
             <p>The purpose of the Avalanche Calculation is to minimize interest paid, and pay off high interest accounts first to eliminate future interest being accrued, opposed to the <a href="{{ route('calculator.snowball.show') }}">Snowball Method</a> - paying off the lowest balances first.</p>
             <p>Sometimes, when you have a lot of debt spread across multiple accounts, you won't see the benefits of the avalanche method until nearer the end of your payments timeline, though the benefit is surely there and is a dynamically increasing factor throughout the entire procedure.</p>
             <p>Because of the nature of the Avalanche Calculator, paying off the highest interest debts first, you may see the payment order of your debts change throughout the timeline - account charges are taken into calculation, and are converted into their APR equivalent, which is then compared to help decide the order.</p>
         </div>
     </div>
+    @auth
+    <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm w-full">
+        <h2 class="flex-1 text-lg font-semibold">Saved Calculations</h2>
+        <div class="w-full mt-4">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Detail
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Total / Count
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Budget
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Load
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach(Auth::user()->calculations()->with(CalculationDebts::class)->get() as $savedCalculation)
+                                <livewire:saved-calculation-row :calculation="$savedCalculation"/>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endauth
     <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm w-full" x-show="open">
         <h2 class="flex-1 text-lg font-semibold">Calculation Wizard</h2>
         <div class="w-full mt-4 flex flex-col space-y-4">
+            <livewire:start-over-button />
             <livewire:calculation-debts-section :calculation="$calculation" />
-            <div class="w-full">
+            <div class="w-full flex flew-row gap-x-6">
                 <a href="{{ route('calculator.results.avalanche.show') }}" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-auto sm:text-sm">
                     Calculate
                 </a>
--- resources/views/calculators/snowball.blade.php
+++ resources/views/calculators/snowball.blade.php
@@ -1,15 +1,52 @@
 <x-app-layout>
     <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm">
-        <h2 class="text-lg font-semibold mb-4">Snowball Method</h2>
-        <div class="flex flex-col space-y-2">
+        <h2 class="text-lg font-semibold">Snowball Calculator</h2>
+        <div class="w-full mt-4 flex flex-col space-y-2">
             <p>The Snowball method of paying off debt, is the most known method world-wide.</p>
             <p>The purpose of the snowball is to eliminate lowest balance debts first, and work your way up to the highest balance debts <span class="font-semibold">linearly</span>. You typically <span class="font-semibold">do pay more interest</span> by using the snowball method, opposed to the <a href="{{ route('calculator.avalanche.show') }}">avalanche method</a>.</p>
             <p>Whilst conquering your debt via the snowball method, you will <span class="font-semibold">feel</span> a quick relief after seeing your smaller debts disappear fast, which seriously helps with motivation to continue on.</p>
         </div>
     </div>
+    @auth
+    <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm w-full">
+        <h2 class="flex-1 text-lg font-semibold">Saved Calculations</h2>
+        <div class="w-full mt-4">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Detail
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Total / Count
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Budget
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Load
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach(Auth::user()->calculations()->with(CalculationDebts::class)->get() as $savedCalculation)
+                                <livewire:saved-calculation-row :calculation="$savedCalculation"/>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endauth
     <div class="bg-white p-4 border border-gray-200 border-solid shadow-sm rounded-sm w-full" x-show="open">
         <h2 class="flex-1 text-lg font-semibold">Calculation Wizard</h2>
         <div class="w-full mt-4 flex flex-col space-y-4">
+            <livewire:start-over-button />
             <livewire:calculation-debts-section :calculation="$calculation" />
             <div class="w-full">
                 <a href="{{ route('calculator.results.snowball.show') }}" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-auto sm:text-sm">
